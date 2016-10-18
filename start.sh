ad8f51747f4695854c5bdc2fa6beea8b3571724b
Felix Brucker <felix.brucker@live.de>
2016-10-18 16:29:38 +0200

    stuff

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [ ! -d "miner-manager" ]; then
+  git clone --branch cloud https://github.com/felixbrucker/miner-manager
+  cd miner-manager
+  npm install
+  npm install pm2 -g
+  pm2 start process.json
+  pm2 save
+  cd ..
+  find cpuminer-opt -maxdepth 0 -empty -exec git clone https://github.com/felixbrucker/cpuminer-opt \;
+  cd cpuminer-opt
+  cp /app/.apt/usr/include/x86_64-linux-gnu/gmp.h .
+  ./build.sh
+  mkdir -p ../miner-manager/bin
+  cp cpuminer ../miner-manager/bin/
+  git reset --hard
+  cd ..
+  sleep infinity
+fi
