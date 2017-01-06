e8f3fcc8d4f199d96b04124b443f2695d84344bf
Felix Brucker <felix.brucker@live.de>
2017-01-06 04:05:24 +0100

    fixes

--- helpers/updateLinuxMiner.sh
+++ helpers/updateLinuxMiner.sh
@@ -5,30 +5,41 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # change to working dir
 cd ..
 
-# clone on first time
+# clone on first time and setup
 if [ ! -d "miner" ]; then
-  git clone https://github.com/felixbrucker/linux-miner miner
-fi
-
-cd "miner"
-
-# check on src changes for building
-prevSizeCPU=`du -s src/cpuminer-opt`
-
-# update
-git pull
-
-# check on src changes for building
-currSizeCPU=`du -s src/cpuminer-opt`
-
-# build incase newer version is detected
-if [ "$prevSizeCPU" != "$currSizeCPU" ]; then
-    echo 'newer cpuminer-opt version available, building ...'
+    git clone https://github.com/felixbrucker/linux-miner miner
+    cd "miner"
+    echo 'initial setup, building ...'
     cd src/cpuminer-opt
     ./build.sh
     mkdir -p ../../../bin
     cp cpuminer ../../../bin/
     cd ../../
     git reset --hard
+else
+
+    cd "miner"
+
+    # check on src changes for building
+    prevSizeCPU=`du -s src/cpuminer-opt`
+
+    # update
+    git pull
+
+    # check on src changes for building
+    currSizeCPU=`du -s src/cpuminer-opt`
+
+    # build in case newer version is detected
+    if [ "$prevSizeCPU" != "$currSizeCPU" ]; then
+        echo 'newer cpuminer-opt version available, building ...'
+        cd src/cpuminer-opt
+        ./build.sh
+        mkdir -p ../../../bin
+        cp cpuminer ../../../bin/
+        cd ../../
+        git reset --hard
+    fi
 fi
+
+
 
